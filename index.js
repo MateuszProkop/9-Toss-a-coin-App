733482fa38eef1c2a1a3b58a8470b0d63ddce1bf
Mateusz_Prokop <mateusz.prokopp@gmail.com>
2021-08-14 12:54:03 +0200

    Game logic feature

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const message = document.querySelector('.info');
 const buttons = document.querySelectorAll('button');
 
 const coinArray = [ 'Heads', 'Tails' ];
+const score = [ 0, 0 ];
 
 for (i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener('click', (e) => {
-		console.log(e.target.innerText);
-		let ComputerToss = Math.floor(Math.random() * 2);
-		console.log(coinArray[ComputerToss]);
+		let playerPick = e.target.innerText;
+		let computerToss = Math.floor(Math.random() * 2);
+		let computerPick = coinArray[computerToss];
+		let output;
+		if (playerPick === computerPick) {
+			output = 'Player Won';
+			score[0]++;
+		} else {
+			output = 'Computer Won';
+			score[1]++;
+		}
+		message.innerHTML = output + '<br>Player ' + score[0] + ' Computer ' + score[1];
 	});
 }
